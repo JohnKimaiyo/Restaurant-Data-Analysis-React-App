e79189c0c3aac67b3d1f04cc51e23020e164e6fd
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-25 21:02:29 +0300

    first commit

---
+++ src/SQL Scripts/dbo.sql
@@ -0,0 +1,11 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [Rank]
+      ,[Restaurant]
+      ,[Location]
+      ,[Sales]
+      ,[YOY_Sales]
+      ,[Units]
+      ,[YOY_Units]
+      ,[Unit_Volume]
+      ,[Franchising]
+  FROM [Restaurant Data].[dbo].[REstaurant data base0$]
